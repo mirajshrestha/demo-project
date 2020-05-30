0cd17117b7d26ff14903f8a9bf9098f13dab8ea2
Miraj Shrestha <mirajshrestha10@gmail.com>
2020-05-30 18:42:19 +0545

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { ListComponent} from "./list/list.component";
+import { HomeComponent} from "./home/home.component";
+
+const routes: Routes = [
+  {path:'', component: HomeComponent},
+  {path:'list', component: ListComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
